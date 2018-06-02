8521c1da7eac4b15a9f91014c1a459fca7651fc9
liannaccone <lciannaccone@gmail.com>
2018-06-02 16:49:14 -0700

    updating model dependencies

--- backend/hunters_log/models.py
+++ backend/hunters_log/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    email = models.EmailField()
-    last_name = models.CharField(max_length=264)
-    first_name = models.CharField(max_length=264)
+from django.contrib.auth.models import User
 
 class Location(models.Model):
     name = models.CharField(max_length=264)
@@ -17,17 +13,36 @@ class Location(models.Model):
         return self.name
 
 class Game(models.Model):
+
+    MAMMAL = 'mammal'
+    BIRD = 'bird'
+    FISH = 'fish'
+
+    GAME_CHOICES = (
+        (MAMMAL, 'Mammal'),
+        (BIRD, 'Bird'),
+        (FISH,'Fish'),
+    )
+
+    MALE = 'male'
+    FEMALE = 'female'
+
+    SEX_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+
     name = models.CharField(max_length=264)
-    classification = models.CharField(max_length=264)
-    sex = models.CharField(max_length=264)
+    classification = models.CharField(max_length=264, choices=GAME_CHOICES)
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES)
 
     def __str__(self):
         return self.name
 
 class Log(models.Model):
-    user = models.ForeignKey(User)
-    location = models.ForeignKey(Location)
-    game = models.ForeignKey(Game)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    game = models.ForeignKey(Game, null=True, on_delete=models.SET_NULL)
     date = models.DateField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
