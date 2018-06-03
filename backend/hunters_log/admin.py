2c4874349b9fd9d197c6f98a93127870d4380f24
liannaccone <lciannaccone@gmail.com>
2018-06-02 17:02:40 -0700

    registering models in admin.py

--- backend/hunters_log/admin.py
+++ backend/hunters_log/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from hunters_log.models import User
+from hunters_log.models import Location
+from hunters_log.models import Game
+from hunters_log.models import Log
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Location)
+admin.site.register(Game)
+admin.site.register(Log)
